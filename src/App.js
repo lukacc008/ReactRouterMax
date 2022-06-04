948d5e388d82d269de16093e44f0040739cb79a0
unknown <lukacc008@gmail.com>
2022-06-04 17:04:44 +0200

    Added links to pages using Link import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { Route } from 'react-router-dom'
 
-import Welcome from './components/Welcome';
-import Products from './components/Products';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
+      <MainHeader />
+      <main>
       <Route path="/welcome">
          <Welcome />
       </Route>
       <Route path='/products'>
         <Products />
       </Route>
+      </main>
     </div>
+    
   );
 }
 
