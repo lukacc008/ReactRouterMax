38e7c74972f57e0fce33451f105671badf89070d
unknown <lukacc008@gmail.com>
2022-06-05 18:25:40 +0200

    NavLink and some styling, used activeClassName aswell

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,19 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-      <header>
-          <nav>
-              <ul>
-                  <li>
-                      <Link to="/welcome">Welcome</Link>
-                  </li>
-                  <li>
-                      <Link to="/products">Products</Link>
-                  </li>
-              </ul>
-          </nav>
-      </header>
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
