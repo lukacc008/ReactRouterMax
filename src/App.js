97e64b54de326e36821f2182ff0f6d088b8b04fd
unknown <lukacc008@gmail.com>
2022-06-06 12:42:07 +0200

    Added dynamic route path with /:productId

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-      <Route path="/welcome">
-         <Welcome />
-      </Route>
-      <Route path='/products'>
-        <Products />
-      </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
       </main>
     </div>
-    
   );
 }
 
